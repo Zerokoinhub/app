e49f9708e5c95299ba655aaa773f0abc1e038c4c
Umairanwarr <umairanwar@gmail.com>
2025-08-07 17:13:48 +0500

    Push project with .env and all changes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,42 +3,43 @@ const admin = require('firebase-admin');
 // Initialize Firebase Admin
 const serviceAccount = {
   "type": "service_account",
-  "project_id": "zerokoin-4e239",
-  "private_key_id": "e35ff1eb557a6fc234773b10460691741ee5bedc",
+  "project_id": "zerokoin-705c5",
+  "private_key_id": "c31432c2318b18ad04f287f2b625c81a4e0d0827",
   "private_key": `-----BEGIN PRIVATE KEY-----
-MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDREazAgqIozN2L
-gA/wSeuG9ZVKaKq2HEP1jJ1gn1zT3pjzjTRmliRg2sI+4YzR35oDomW6BKn/0nWa
-UA3AUhp0/37A9P613ugeA5mu4NM0/3eVhVf+/R+5S+XCKxsMJs4xd+YAdF2Yd+MK
-KLlZv14ZaoQddFITbRz3JKbdBJxTwjWnZGL/4SdCpksDcJai6O9n4iY02/0yhRMo
-OtBe563A12zlAlrkMODWGe0HR5d89gTHdUnWLkMwmS2IUjTVkXK2yWx6arff2Dg4
-IjublErikvZt6evN19RpkivK0GiJnNAc1HzOAdqDZ1Q0kStj68jSsAE9yCvSrWjV
-+dciZ1tPAgMBAAECggEAIsKAXojO+DSeLbLkM4G+wzNdQmqmM11TXpB7RqodpMDb
-h5F1twwFzq/1tWm1Ctx/0/8x+vG1PVTa+7CNUL6gf8z44JD7MPbM0IIDEQ8iiSsO
-UVGYYKCUyoSCwJEqeo87H1YSwLGYMXYr99pEfYDyiWGVx1CPKC6vLwICLONhhbqK
-i5QHen85QuGbLKGkdU1jREY43HkDpbO+7soKScVdGLunTTkkogvpafN2mTLCzCs6
-OqNtjmVzaEGF3eJeJnc3ef+no8bx/CY5v5Grif1yR8XZ5QhUmv9590rjtBI7bHPH
-JT3SdK3z8/uJ9axP1Y99lC/q16MqyHYXqD+N49b5EQKBgQD1g/2S2Qz3G+sfxTnj
-YllUuE922hW/Q/VqWbzW81hmX+NFBUBkr98ERNbrU+mS0U1WmppCp+OTYuttTbNi
-+kl+KdMmRFfFDp9+55HSRgKOpQGcp/TUNj9e51p3Ms9aS4FmSk/OcqzZXmu5Sfvs
-AJA+1ivQjvXVj6vLVXy/3w3b0QKBgQDZ/z7pBx9IPhB04YY7HEX9jQFIO46Zpzr/
-ahLEXr5ocekv2nP/pQubK3eFBPWJzcS3M0QRQJoEQOO6BFGvvhef4RS7TDQu4vZ6
-yPWtlP83neFheXA3PEdASYxgNNPYMW5UvE4UQp0zMHPG5qEpvq5PjylLDN6xadl8
-3i0CsjYtHwKBgQDX5tp26KieJc7+gVIGw9YKyooW2nGFYR3QCFooLJbwcCJL7/JQ
-mNhlyKO3DnO4yJZaMlq1gy59zd96n3nA8JjweZdVb4Q/pjxcPQffAfH9vt1MBecn
-Nw1DR/AmImh13zBL482Knw2hmYbhyk3fnB89Itok7ZMB2vYYOXUaxexKAQKBgFJo
-2phvN9UwmToGM59O7eqPDogG876EaJo7uEKB6otDXLrZY3amJwAO64zetg/VOONk
-vvuip1aqTSx6C4NbYkTvas3vXd7HsBP0umNXay/UQGp/5PsMj4bvBuErt1YIQcRI
-t1+TWygJzgrIDu7gBbdP0HVttRXuhOheeTeGtVJTAoGBALpf8zZ9vldue0BFr099
-tBqbL3nZfI9YtQNVnES9tyxkDKr3JEL0N9hVT2mZBIVYnbDWcRuTg2ftLAnDXXz2
-/X+heA6FDUacSIZAil3qv+2PkHvz8NVK/dDQPrXL+pBT5HY8VMTikPp+UdpO/kyn
-NlvxmkjuRsvHPkCZHHv9cxfq
+MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCvHmTumKriwT88
+G2fb3pu0uh+COoIhUXzlz2lVXmmKF5bI0/rDFfmIa8XXm34XZg4dimKMXTsXqY5L
+Bezjk1iY42TFGv3k5SjS0aUbIwO2XbxlaZYfh4WM2r+qajfkz5JTu+RFrQ/Xt1gm
+gB2xqt+Um5OG+TUsT/SHuAhtQxIBPFCwafsUMVMXz53csMd1SDeVlGnactBVQdfm
+7cBLjbfMa0TAWK4+oqwb2x6TFBlP0OHFgQxYU55/C+W9zIMp0AnoVydGrUQ3oqWz
+pDLl/Jn/PANjYgyR1oa1vd+MI8V118NKwssPmWwdBbYBCQm/a9lJL2QgWGTE6G/t
+mDYnihhxAgMBAAECggEABqcQQSjtNX7vRbuaDAYSeti3A5fQy56ckL+q1gyXlHCZ
+2KRmbmi4XYKQHhUs8qs8qipAnQnZ6cfGBUM67QiTBSBe2rbQOkTLIqqmk7/GV2RL
+WNwscQNRS1EAVuNQ/yUUT1BoDy5QV7LiMzi64A3wb1Ujcy1GlAnPzaPb9JgnoAK0
+KYfCz9CXMuRMLqpTplfCrh/VdVpxMDmWUMMY2yDRUJTuEm/9kUUnLAIBAm6sM021
+PP9ZkLxkW8GmO1UjiHuT8DFlYRts3uLKWUf1RHhWztY3cPo0m7TnAKWg1MU+fZ5X
+qO129f7SAZ/Xp0Vju8UtxjLtqnu4OvWyjSkXVaauoQKBgQDciC8mgDSgwOWF+cee
+6tkMuoQQPoUByMs7R25f0/ewNXc63xy6yTuYVbuWGrThqVyDe0US0wsDuDYID1B3
+v3DtQmMpIB1KM8vnjlr5H7ZotEJoTWY9mvZQU2N//CbuRu17MthNda99AYB0hxMN
+NNS8T2LdjwPLW94QWhVXjlSZkQKBgQDLSHDhdjxoW/3iwXxOTgV8zjjmM66Q0VcM
+DGqm7Go9JUtpE5RXOP8aLxNg93Gg5/GVpWXp/4vAfdENv09CWylS33zGClt2IwDy
+fReOfwM0xvjU7H3pJ616tlI6CsPKkb1qTLdaf0+vTEZUuZkixONDQ79Jv/dwGf7W
+LDqd5a0g4QKBgQCXKnZ/gqQQiQkL1BKkeUC8u+EorWJIvdWpVaZv44FF9PA8l5E+
+7A+AtUu3cakJgikbK1VGuQk0zIGk9rJm0wBacgY8u3m4Ulbz652qZLWJgf5/Nobd
+taofl3l90l7xOAczSsAAIKphGrVtquVBSTSFvDj9vDmSnO9T8Bvu/244MQKBgQDF
+cs9xrw5tryyjG5xJ0q6x0vyhXIGqWCtcoJLXH7rxOVBN956WOd5O6HrCLJZJ3ABj
+ggvDhVuQ1KRB3hh2M2WoATuIgHbmAIni/m3rEqNimAEJj8ucs9Vk48JqbhpOn9CD
+d5CAbjhP5J5LnRqJEwRLSXp7f+IvGJDWJo+o3nWTAQKBgQCq44S2N7p7TRxJjmSG
+GD9k0Q1fVed5avIWcFGLCmPFAB3xWKWiMM6DtnNviXsF96g9g0Ek6nCynCk/Cc5e
+MBwwk6pDDrG5To8jieTCjUeBevhOQE0SEZpKpuOHJW4wXr+5b/SUjiPaCguSPMcu
++Z8zFeUbnXnP8vwkrqTmo8iWjg==
 -----END PRIVATE KEY-----`,
-  "client_email": "firebase-adminsdk-fbsvc@zerokoin-4e239.iam.gserviceaccount.com",
-  "client_id": "108196235902460709232",
+  "client_email": "firebase-adminsdk-fbsvc@zerokoin-705c5.iam.gserviceaccount.com",
+  "client_id": "108588023842407313656",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40zerokoin-4e239.iam.gserviceaccount.com"
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40zerokoin-705c5.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
 };
 
 // Initialize Firebase Admin SDK
